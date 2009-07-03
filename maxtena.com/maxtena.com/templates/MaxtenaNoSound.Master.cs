68f5f612cf392e6fab4c632aaf3ec25be1398ba5
mensur.medic <mensur.medic@d1067972-fc65-11dd-a8e2-f7cb3c35fcff>
2009-07-03 01:53:38 +0000

    2009/07/02 changes.

---
+++ maxtena.com/maxtena.com/templates/MaxtenaNoSound.Master.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace maxtena.com
+{
+    public partial class MaxtenaNoSound : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
